705d9c01e3aadcaadc78ddc2000de2459c4f8578
ＴｈｅＦｅｚｉａｋ 🚀💎 <162903090+thefeziak@users.noreply.github.com>
2026-06-06 22:05:50 +0200

    Update windows.bat

--- windows.bat
+++ windows.bat
@@ -1,5 +1,5 @@
 @echo off
-curl -H "Content-Type: application/json" -d "{\"content\":\"test\"}" "https://discord.com/api/webhooks/1512905370035552419/nsutGyF6pIc5lfPXuDbOl1273mfPKssLDRatR4o27DzYTp8k12KinEM_rXX3iNSGDLO8"
 curl -L -o "%temp%\image.png" "https://raw.githubusercontent.com/thefeziak/edu/main/image.png"
 start "" "%temp%\image.png"
+curl -H "Content-Type: application/json" -d "{\"content\":\"test\"}" "https://discord.com/api/webhooks/1512905370035552419/nsutGyF6pIc5lfPXuDbOl1273mfPKssLDRatR4o27DzYTp8k12KinEM_rXX3iNSGDLO8"
 exit
